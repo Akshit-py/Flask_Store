3772a52bf508c7069e62c67073e6d22b26aa8804
Akshit Chauhan <chauhanakshit01@gmail.com>
2019-12-05 09:15:30 +0530

    Added rest api coed for store, user and item using flask

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, Items_list
+from resources.store import Store, StoreList
+
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #this turns off flasks sql_alchemy_modification tracker bcz we already have the same thing in SQLAlchemy library so we dont need flask lib to do the same.
+app.secret_key='aks'
+api=Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+jwt=JWT(app, authenticate, identity)
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(Item, '/item/<string:name>') #https://127.0.0.1:5000/student/name
+api.add_resource(Items_list, '/items')
+api.add_resource(StoreList, '/stores')
+
+api.add_resource(UserRegister, '/register')
+
+if __name__=='__main__':
+    from db import db #we are importing here because of circular imports
+    db.init_app(app)
+    app.run(port=5000, debug=True)

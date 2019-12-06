f3a832076dff3c38f67a88cd289aefad8cda37bd
Akshit Chauhan <chauhanakshit01@gmail.com>
2019-12-06 09:55:49 +0530

    adding run.py for heroku

--- app.py
+++ app.py
@@ -13,9 +13,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #this turns off flasks sql_
 app.secret_key='aks'
 api=Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+
 
 jwt=JWT(app, authenticate, identity)
 
